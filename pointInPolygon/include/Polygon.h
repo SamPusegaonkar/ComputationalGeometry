c45deff776525c1b1947bb2e7ab738c3d2b78d2f
Sameer Pusegaonkar <sameer@umd.edu>
2022-02-04 21:35:00 -0500

    Changed input parameter of readFile

--- pointInPolygon/include/Polygon.h
+++ pointInPolygon/include/Polygon.h
@@ -4,7 +4,7 @@
 class Polygon : public Edge {
     public: 
     std::vector<std::unique_ptr<Edge>> edges;
-    void readFile();
+    void readFile(std::string filename);
     std::vector<std::unique_ptr<Edge>> getPoints();
     int isPointInPolygon();
     int intersectEdge(std::unique_ptr<Edge> e, std::unique_ptr<Point> p,int m);
