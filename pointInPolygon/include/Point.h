124c7c31af950afe1f97302667cd5662949f357c
Sameer Pusegaonkar <sameer@umd.edu>
2022-02-04 21:34:39 -0500

    Removed unncessary file

--- pointInPolygon/include/Point.h
+++ pointInPolygon/include/Point.h
@@ -5,9 +5,6 @@ class Point {
     public:
     double x, y;
     Point( double X, double Y );
-    bool operator==(const std::unique_ptr<Point>& other) const;
     bool operator==(const Point& other) const;
 
-
-   
 };
