270f74931daf6b90a65533f227a8dd0091024d3e
spusegaonkar <sameer.pusegaonkar@quanergy.com>
2022-04-23 17:37:00 -0700

    Fixed multipler value for linear interpolation

--- triangleQuadTree/partTwo/grid.py
+++ triangleQuadTree/partTwo/grid.py
@@ -60,6 +60,83 @@ class Grid:
         has_pos = (val1 > 0) or (val2 > 0) or (val3 > 0)
         return not (has_neg and has_pos)
 
+    def getIntersectingVertices(self, tin, grid_vertex):
+
+        for triangle_index in grid_vertex.triangles_indices:
+            triangle = tin.get_triangle(triangle_index)
+            vertex_indices = triangle.get_vertex_indices()
+
+            vertex_1 = tin.get_vertex(vertex_indices[0])
+            vertex_2 = tin.get_vertex(vertex_indices[1])
+            vertex_3 = tin.get_vertex(vertex_indices[2])
+
+            if ( self.isPointOnEdgeHelper(vertex_1, vertex_2, grid_vertex) ):
+                return [vertex_1, vertex_2] 
+
+            if ( self.isPointOnEdgeHelper(vertex_2, vertex_3, grid_vertex) ):
+                return [vertex_1, vertex_2] 
+
+            if ( self.isPointOnEdgeHelper(vertex_3, vertex_1, grid_vertex) ):
+                return [vertex_1, vertex_2] 
+        return []
+    
+    def isPointOnEdgeHelper(self, vertex_1, vertex_2, grid_vertex):
+
+        x1 = vertex_1.get_x()
+        y1 = vertex_1.get_y()
+        p1 = [x1,y1]
+
+        x2 = vertex_2.get_x()
+        y2 = vertex_2.get_y()
+        p2 = [x2,y2]
+
+        x4 = grid_vertex.x
+        y4 = grid_vertex.y
+        p3 = [x4, y4]
+
+        #Collinear
+        if self.getOrientation(p1, p2, p3) == 0:
+            if p2[0] >= p3[0] >= p1[0] and p2[1] >= p3[1] >= p1[1] :
+                return True
+            elif  p1[0] >= p3[0] >= p2[0] and p1[1] >= p3[1] >= p2[1]:
+                return True
+        return False
+
+
+    def setZElevation(self, vertices, grid_vertex):
+        
+        vertex_1, vertex_2 = vertices
+        x1 = vertex_1.get_x()
+        y1 = vertex_1.get_y()
+        z1 = vertex_1.get_z()
+        p1 = [x1,y1,z1]
+
+        x2 = vertex_2.get_x()
+        y2 = vertex_2.get_y()
+        z2 = vertex_2.get_z()
+        p2 = [x2,y2,z2]
+
+        x4 = grid_vertex.x
+        y4 = grid_vertex.y
+        p3 = [x4, y4]
+
+        print(p1, p2)                
+        d = self.getEuclidianDistance(p1, p3)
+        print(d, "d", p1[0]*p3[0] + p1[1]*p3[1])
+        v = [p2[0] - p1[0], p2[1] - p1[1], p2[2] - p1[2]]
+        print(v)
+        mag_v = self.getMagnitude(v)
+        print(mag_v, "V magnitude")
+        multiplier = d/mag_v
+        print(multiplier)
+        grid_vertex.z = p1[2]+ multiplier* v[2]
+        return None
+
+    def getEuclidianDistance(self, p1, p2):
+        return math.sqrt((p1[0]- p2[0])**2 + (p1[1]-p2[1])**2)
+
+    def getMagnitude(self, x):
+        return math.sqrt(x[0]* x[0] + x[1]*x[1] + x[2]*x[2])
 
     def getOrientation(self, p1, p2, p3):
 
@@ -98,10 +175,14 @@ class Grid:
                         print("point not inside tin")
                     print("``````````````````````````````````````")
 
-                # onEdge, z_elevation = self.isOnEdge(tin, grid_vertex)
-                onEdge, z_elevation = False, 0.0
-                if (onEdge):
-                    grid_vertex.z = z_elevation
+
+                # If the point is on an edge
+                vertices = self.getIntersectingVertices(tin, grid_vertex)
+                if len(vertices): 
+                    print("found", len(vertices))
+                    self.setZElevation(vertices, grid_vertex)
+                    print(grid_vertex.z)
+
                 else:
                     grid_vertex.z = self.getElevationHelper(grid_vertex, tin)
     
@@ -144,7 +225,9 @@ class Grid:
             return z4
 
 #starting point x, y, grid cell size (height and width), number of rows, and number of cols.
-g = Grid(45,45,20,20,2,2)
+# g = Grid(45,45,20,20,2,2)
+g = Grid(67, 200,20,20,2,2)
+
 g.getElevations()
 
 for i in range(len(g.grid)):
